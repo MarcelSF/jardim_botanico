bdba3851ae898ddab36dbadfda8f3044791d07db
Marcel Fonseca <marcelsf23br@gmail.com>
2020-05-27 18:24:55 -0300

    day one done :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying all gardens"
+
+Garden.destroy_all if Rails.env.development?
+
+puts "creating a couple of gardens"
+
+Garden.create!(
+    name: "My cool garden",
+    banner_url: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80"
+)
+
+Garden.create!(
+  name: "Milene's Garden!",
+  banner_url:"https://images.unsplash.com/photo-1483809715206-0499044b5b69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+)
+
+puts "Done!"
